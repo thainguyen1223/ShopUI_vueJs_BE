2a62d4bdf95560606c68425e3bb65ffbc013cc69
your name <duythai147258@gmail.com>
2023-06-15 02:46:07 +0700

    dsadsa

--- src/require-clothes/require-clothes.service.ts
+++ src/require-clothes/require-clothes.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
-import { RequireClothes, RequireClothesDocument } from "src/require-clothes/schemas/require-clothes";
 
 import { Model, ObjectId } from "mongoose";
 import { CreateRequireClothesDto } from "./dto/create-require-clothes-dto";
 import { UpdateRequireClothesDto } from "./dto/update-require-clothes-dto";
+import { RequireClothes, RequireClothesDocument } from "./schemas/require-clothes";
 @Injectable()
 export class RequireClothesService {
   constructor(@InjectModel(RequireClothes.name) private RequireClothesModel: Model<RequireClothesDocument>) {}
